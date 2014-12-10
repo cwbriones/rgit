2041640de5057258ad9b39597e9b3ef94da49795
Christian Briones <christian@whisper.sh>
2014-12-09 21:09:42 -0800

    add rudimentary CLI

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,37 @@
+extern crate getopts;
+
+use std::os;
 use std::io::IoResult;
 
 mod tcpclient;
 
 fn main() {
-    match ls_remote("127.0.0.1", 9418, "rgit") {
-        Ok(_) => {
-            println!("refs obtained");
+    let args: Vec<String> = os::args();
+    let program = args[0].clone();
+
+    if args.len() > 1 {
+        let status_code = run_command(&args[1], args.slice_from(2));
+        os::set_exit_status(status_code);
+    } else {
+        let usage =
+            "usage: rgit <command> [<args>]\n\n\
+            Supported Commands:\n\
+            ls-remote <repo>           List references in a remote repository\n";
+        print!("{}", usage);
+    }
+}
+
+fn run_command(command: &String, args: &[String]) -> int {
+    match command.as_slice() {
+        "ls-remote" => {
+            ls_remote("127.0.0.1", 9418, "rgit");
+            0
         },
-        Err(_) => {
-            println!("Error!");
+        unknown => {
+            println!("Unknown command: {}", unknown);
+            -1
         }
-    };
+    }
 }
 
 fn pktline(msg: &str) -> String {
